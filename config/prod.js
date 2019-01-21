c00119b1f085773bdfa11853029700a161b9d480
Guillermo <williamchoudev@gmail.com>
2019-01-21 16:36:37 -0400

    update

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,8 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   redirectDomain: process.env.REDIRECT_DOMAIN,
-  // googleCallbackURL: 'https://sportid6.herokuapp.com/auth/google/callback',
-  googleCallbackURL: 'http://www.creando-nodejs.com/auth/google/callback',
+  googleCallbackURL: 'https://sportid6.herokuapp.com/auth/google/callback',
+  // googleCallbackURL: 'http://www.creando-nodejs.com/auth/google/callback',
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
 };
