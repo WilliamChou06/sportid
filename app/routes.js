945279099ee828d8f65c4b9d44dc9a9f206fc9fd
Guillermo <williamchoudev@gmail.com>
2019-01-21 16:33:45 -0400

    update

--- app/routes.js
+++ app/routes.js
@@ -60,7 +60,7 @@ cloudinary.config({
 module.exports = (app, passport) => {
   app.get('/', (req, res) => {
     if (process.env.NODE_ENV === 'production') {
-      var googleAuthLink = 'https://sportid6.herokuapp.com/auth/google';
+      var googleAuthLink = 'http://www.creando-nodejs.com/auth/google/callback';
     } else {
       var googleAuthLink = 'http://localhost:8080/auth/google';
     }
